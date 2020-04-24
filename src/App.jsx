2b3bad6fda2a15ec1a8e4d1a2142ea03188effc1
Aleksandr Puskov <oleksandrp@localize.city>
2020-04-24 17:37:27 +0300

    init main

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './main';
+import { Content } from './styled';
+
+function App() {
+  return (
+    <Content>
+      <Main />
+    </Content>
+  );
+}
+
+export default App;
---
+++ src/components/control-panel/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ControlPanel = () => {
+  return (
+    <div>
+      ControlPanel
+    </div>
+  );
+};
+  
+export default ControlPanel;
+  
